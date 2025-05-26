cb8e7c45d23aafad5be0f891579ece22e10d1eea
Achrosy <rosy.klein19@gmail.com>
2025-05-26 11:07:38 +0545

    Login screen is added,This is only a SplashScreen,

--- app/src/main/java/com/example/appsplashscreen/MainActivity.kt
+++ app/src/main/java/com/example/appsplashscreen/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.appsplashscreen
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -8,12 +11,27 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var loginBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         Thread.sleep(3000)
         installSplashScreen()
         setContentView(R.layout.activity_main)
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener({
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i("test Credentials", "username: $username and password: $password")
+        })
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
